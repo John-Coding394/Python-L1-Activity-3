ff3cc4bd99dab596c865fe33b5d496d30b374a03
John-Coding394 <johnchaghouri394@gmail.com>
2025-04-12 10:22:54 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Hello world")
+print(5)
+print("Hello Python \n")
+print("hello",5)
+print("Welcome to", end="*")
+x = 5
+y = "John"
+print(x)
+print(y)
+num = input("Pick a number ")
+name = input("What is your name? ")
+print("\nHello", name, "\nwelcome to Codingal")
+print(num)
+import keyword
+print(keyword.kwlist)

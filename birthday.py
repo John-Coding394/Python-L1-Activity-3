0eebe36000f21832d1e858906450cf3374a203a7
John-Coding394 <johnchaghouri394@gmail.com>
2025-04-12 10:27:50 -0400

    Second commit

---
+++ birthday.py
@@ -0,0 +1,6 @@
+b1 = input("Put in a birthday ")
+b2 = input("Put in a birthday ")
+b3 = input("Put in a birthday ")
+b4 = input("Put in a birthday ")
+b5 = input("Put in a birthday ")
+print("Ok so your 5 birthdays are", b1 , b2 , b3 , b4 , b5)
